3aa8d4534f341841b0e90c2481432a21995d454e
Alex Padilla <alex.padilla@epn.edu.ec>
2022-11-13 21:56:14 -0500

    Barrels implementation

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './header'
+export { default as Hero } from './hero'
+export { default as About } from './about'
+export { default as WorkExperience } from './experience'
